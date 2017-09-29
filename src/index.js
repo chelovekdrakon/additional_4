77c4eb8e4fdd83f18645b60d0b760b3a75f9cc25
chelovekdrakon <MorauFiodar@yahoo.com>
2017-09-29 12:06:23 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+
+  if (parseInt(first) === 0 || parseInt(second) === 0) {
+         return '0';
+     }
+
+   first = first.split('').reverse();
+   second = second.split('').reverse();
+   let product = [];
+
+   for (let i = 0; first[i] >= 0; i++) {
+       for (let j = 0; second[j] >= 0; j++) {
+           if (!product[i + j]) {
+               product[i + j] = 0;
+           }
+
+           product[i + j] += first[i] * second[j];
+       }
+   }
+
+   for (let i = 0; product[i] >= 0; i++) {
+       if (product[i] >= 10) {
+           if (!product[i + 1]) {
+               product[i + 1] = 0;
+           }
+
+           product[i + 1] += parseInt(product[i] / 10);
+           product[i] %= 10;
+       }
+   }
+
+   return product.reverse().join('');
+ }
